8a9cccc7675aaf42a453559099efefe662c8db9a
Marian Meres <marian@meres.sk>
2020-08-06 19:36:58 +0200

    wip

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,13 @@ from dotenv import load_dotenv
 from .args_utils import parse_cli
 from .file_utils import read_files
 
+# kvazi constants... hm...
+DB_HOST = "DB_HOST"
+DB_USER = "DB_USER"
+DB_PASSWORD = "DB_PASSWORD"
+DB_DATABASE = "DB_DATABASE"
+DB_PORT = "DB_PORT"
+
 
 def create_logger(is_silent):
     def _logger(*args):
@@ -24,17 +31,10 @@ def main():
         # laod .env... @see parse_cli
         load_dotenv(_args.env)
 
-        # kvazi "consts" hm...
-        DB_HOST = "DB_HOST"
-        DB_USER = "DB_USER"
-        DB_PASSWORD = "DB_PASSWORD"
-        DB_DATABASE = "DB_DATABASE"
-        DB_PORT = "DB_PORT"
-
         # collect db config envs
-        db_cfg_keys = [DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT]
-        db_cfg = {key: os.getenv(key) for key in db_cfg_keys}
-        # _logger(db_cfg)
+        db_cfg = {key: os.getenv(key) for key in [
+            DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT
+        ]}
 
         is_production = os.getenv("PYTHON_ENV", "production") == "production"
         if is_production and not _args.force:
@@ -43,7 +43,7 @@ def main():
             )
             sys.exit(0)
 
-        # maybe do not continue...
+        # maybe exit...
         if not _args.yes:
             yn = input(
                 f"\nThis will recreate sql schema in "
@@ -52,9 +52,12 @@ def main():
             ).strip()
             if not re.match(re.compile("^y(es)?$", re.IGNORECASE), yn):
                 _logger("Bye, nothing to do...")
-                return
+                sys.exit(0)
+
+        # read what we have
+        files = read_files(_args.indir)
+        # _logger(files)
 
     except KeyboardInterrupt:
         sys.exit(0)
 
-    # files = read_files(args.indir)
--- src/file_utils.py
+++ src/file_utils.py
@@ -1,6 +1,6 @@
 import os
 
 
-def read_files(from_directory):
-    print(os.getcwd())
+def read_files(from_directory: str, ext: str = 'py'):
+    # print(os.getcwd())
     print(os.listdir(from_directory))
